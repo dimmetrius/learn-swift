53b5694e9261f275a52ad80d8bb081bcb87ed900
dimmetrius <topsoft@MacBook-Pro.local>
2015-03-29 16:21:40 +0300

    swift playgrounds

---
+++ hello.playground/section-1.swift
@@ -0,0 +1,45 @@
+// ------------------------------------------------------------------------------------------------
+// Things to know:
+//
+// * Type Aliases allow you to provide a different name for types,
+//   similar to 'typedef' in C.
+// ------------------------------------------------------------------------------------------------
+
+
+
+let maximumNumberOfLoginAttempts : Int = 100
+
+var SomeDouble : Double = 4.0
+
+let fiveHUndred = 500
+
+println("The current value of fiveHundred is: \(fiveHUndred)")
+
+var foo1 = 0
+var foo2 = 0
+
+let meaningOfLife : UInt8 = 42
+let meaningOfLife32 : UInt32 = 42
+
+let pi: Double = 3.14
+let pie: Float = 100
+
+let someString = "SomeString"
+
+let btrue = true
+let bfalse = false
+
+let binaryInteger = 0b10001
+
+
+let simpleInt = 2_000
+
+let one: UInt8 = 1
+
+let x = 2 + UInt16(one)
+
+let x2 = pi + Double(one)
+
+let x3 = 3 + 0.14
+
+
